2bf9569427dd47fa52086af716af57d29aa0ad53
MAsadAnwaar <98406037+MAsadAnwaar@users.noreply.github.com>
2022-07-16 21:02:26 +0500

    Update product.py

--- store/models/product.py
+++ store/models/product.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     old_price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=200, default='' , null=True , blank=True)
-    image = models.ImageField(upload_to='uploads/products/')
+    image = models.ImageField(upload_to='store/static/uploads/products/')
 
     @staticmethod
     def get_products_by_id(ids):
